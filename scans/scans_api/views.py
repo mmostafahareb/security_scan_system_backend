c8fa3b8843734653a5ebe60daa88945e95064c9a
sapomh <61649616+sapomh@users.noreply.github.com>
2023-05-06 07:18:47 +0300

    initial

--- scans/scans_api/views.py
+++ scans/scans_api/views.py
@@ -11,10 +11,10 @@ from rest_framework.exceptions import PermissionDenied
 from .models import *
 from .serializers import *
 from .utils import *
-
+import time
 class UsersViewSet(viewsets.ModelViewSet):
     queryset = Users.objects.all()
-    serializer_class = UsersSerializer
+    serializer_class = UsersSerializerh
 
 class TeamsViewSet(viewsets.ModelViewSet):
     queryset = Teams.objects.all()
@@ -52,24 +52,34 @@ class ProjectsViewSet(viewsets.ModelViewSet):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     @action(detail=False, methods=['post'])
-    def create_from_zip(self, request):
-        zip_file = request.FILES.get('zip_file')
-        project_id = request.data.get('project_id')
+    def create_from_git(self, request):
+        git_url = request.data.get('git_url')
+        project_directory = os.path.basename(git_url.rstrip('.git/').rstrip('/'))
 
-        if not zip_file or not project_id:
-            return Response({"error": "zip_file and project_id are required fields"}, status=status.HTTP_400_BAD_REQUEST)
-
-        try:
-            project = Projects.objects.get(pk=project_id)
-        except Projects.DoesNotExist:
-            return Response({"error": "Project not found"}, status=status.HTTP_404_NOT_FOUND)
-
-        # Save the zip file temporarily
-        temp_zip_file = default_storage.save(f'tmp/{zip_file.name}', zip_file)
-
+        # ...
         with tempfile.TemporaryDirectory() as temp_dir:
             try:
-                with zipfile.ZipFile(temp_zip_file, 'r') as zf:
+                Repo.clone_from(git_url, temp_dir)
+            except Exception as e:
+                return Response({"error": f"Error cloning the repository: {str(e)}"},
+                                status=status.HTTP_400_BAD_REQUEST)
+
+            # Your logic for processing the cloned repository goes here
+            # For example, you can store the repository files or extract relevant information
+
+        project = Projects.objects.create(project_id=int(time.time()), project_directory=project_directory)
+        serializer = ProjectsSerializer(project)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @action(detail=False, methods=['post'])
+    def create_from_zip(self, request):
+        zip_file = request.FILES.get('zip_file')
+        project_directory = os.path.splitext(zip_file.name)[0]
+
+        # ...
+        with tempfile.TemporaryDirectory() as temp_dir:
+            try:
+                with zipfile.ZipFile(zip_file, 'r') as zf:
                     zf.extractall(temp_dir)
 
                 # Your logic for processing the unzipped files goes here
@@ -78,8 +88,9 @@ class ProjectsViewSet(viewsets.ModelViewSet):
             except Exception as e:
                 return Response({"error": f"Error unzipping the file: {str(e)}"}, status=status.HTTP_400_BAD_REQUEST)
             finally:
-                default_storage.delete(temp_zip_file)
+                default_storage.delete(zip_file)
 
+        project = Projects.objects.create(project_id=int(time.time()), project_directory=project_directory)
         serializer = ProjectsSerializer(project)
         return Response(serializer.data, status=status.HTTP_200_OK)
     @action(detail=True, methods=['post'])
@@ -90,11 +101,17 @@ class ProjectsViewSet(viewsets.ModelViewSet):
             return Response({"error": "Invalid project ID"}, status=status.HTTP_400_BAD_REQUEST)
 
         try:
-            Projects.objects.get(pk=project_id)
+            project = Projects.objects.get(pk=project_id)
         except Projects.DoesNotExist:
             return Response({"error": "Project not found"}, status=status.HTTP_404_NOT_FOUND)
 
-        scan_response = scan_project(project_id)
+        # Create a new Scans object
+        scan = Scans.objects.create_scan(project)
+
+        # Set the directory attribute based on the project object
+        directory = project.directory
+
+        scan_response = scan_project(scan, directory)
         return Response(scan_response.data, status=status.HTTP_200_OK)
 class ScansViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Scans.objects.all()
